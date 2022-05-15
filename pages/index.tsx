2ff87dcc4a9354d9f9f275e75072e4fd1bfee7d1
masa5555 <masa57887@gmail.com>
2022-05-15 20:30:40 +0900

    TaskListが更新されていなかった

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { Task } from '../src/types/Task'
+import { useState } from 'react'
 import Header from '../src/components/Header'
 import TaskAdd from '../src/components/TaskAdd'
 import TaskList from '../src/components/TaskList'
 
 const Index = (): JSX.Element => {
+  const [taskList, setTaskList] = useState<Task[]>([])
+  
   const addTask = (task: Task) => {
-    taskList.push(task)
+    setTaskList([...taskList, task])
     console.log(taskList)
   }
-  const taskList: Task[] = []
 
   return (
     <div>
