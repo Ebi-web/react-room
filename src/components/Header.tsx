2da02f7a478b70825f008781cc1f7742a5cba7e2
けんし <kensiiwasaki@gmail.com>
2022-05-22 16:53:52 +0900

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-import { FC } from "react";
+import { FC } from 'react'
 
 const Header: FC = () => {
   return (
     <header className="p-5 bg-blue-300">
-      <p className="items-center">React ToDo</p>
+      <p className="items-center">React ToDo test</p>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
